80bbb38ce6bb57b2e144a662c5d413fd51958520
Marikuana <marians22092000@gmail.com>
2022-05-05 03:04:59 +0300

    Bullet v0.6

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
-    public Vector2 movement;
+    public Vector3 movement;
     private Vector3 spawnPosition;
     [SerializeField] private float destroyDistance = 10f;
     private SpriteRenderer spriteRenderer;
@@ -23,44 +24,77 @@ public class Bullet : MonoBehaviour
         float distance = speed * Time.deltaTime;
         Vector3 direction = movement.normalized;
 
+        int i = 0;
+
         while (distance > 0f)
         {
             Vector2 normal = Vector2.zero;
-            float disToHit = 0f;
+            float disToHit;
 
-            RaycastHit2D hit = Physics2D.CircleCast(position + direction * 0.01f, 0.1f, direction, distance);
+            RaycastHit2D hit = Physics2D.CircleCast(position + direction * 0.1f, 0.1f, direction);
+
             if (hit != default(RaycastHit2D))
             {
                 disToHit = hit.distance;
                 normal = hit.normal;
-                if (hit.collider.TryGetComponent(out IDamageable damageable))
+
+                Vector3 contactPoint = position + direction * disToHit + direction * 0.1f;
+                RaycastHit2D hit2 = Physics2D.Raycast(contactPoint, Average(Vector2.Reflect(direction, normal), direction), 0.1f);
+                if (hit2 != default(RaycastHit2D))
+                {
+                    normal = Average(normal, hit2.normal);
+
+                    if (hit2.collider.TryGetComponent(out IDamageable damageable1))
+                    {
+                        damageable1.Damage(1);
+                    }
+                }
+
+                if(hit.collider.TryGetComponent(out IDamageable damageable))
                 {
                     damageable.Damage(1);
                 }
 
                 SetColor(Random.value > 0.5f ? Color.yellow : Color.blue);
+
             }
             else
             {
                 disToHit = distance;
             }
 
-            position += direction * disToHit;
+            position += direction * disToHit + direction * 0.1f;
 
             distance -= disToHit;
 
             if (normal != Vector2.zero)
-                direction = Vector3.Reflect(direction, normal).normalized;
+            {
+                Vector3 newDirection = Vector3.Reflect(direction, normal);
+                if (newDirection == direction)
+                    direction = Average(direction, normal);
+                else if (newDirection == Vector3.zero)
+                    direction *= -1;
+                else
+                    direction = newDirection;
+
+            }
+
+            if (++i >= 100)
+            {
+                SetColor(Color.black);
+                Debug.LogError(null);
+            }
         }
 
         transform.position = position;
-        movement = direction;
+        movement = direction.normalized;
 
         if (Vector3.Distance(spawnPosition, transform.position) > destroyDistance)
         {
             Destroy();
         }
     }
+
 
     public void ChangeMovement(Vector3 normal)
     {
@@ -84,9 +118,11 @@ public class Bullet : MonoBehaviour
     }
 
     [SerializeField] private float gizmosDistance = 5f;
+    [SerializeField] private int gizmosMaxContact = 1;
 
     private void OnDrawGizmosSelected()
     {
+        StringBuilder log = new StringBuilder();
         int i = 0;
         Gizmos.color = Color.yellow;
 
@@ -94,36 +130,79 @@ public class Bullet : MonoBehaviour
         float distance = gizmosDistance;
         Vector3 direction = movement.normalized;
 
+
         while (distance > 0f)
         {
             Vector2 normal = Vector2.zero;
             float disToHit;
 
-            position = position + direction * 0.1f;
-            Gizmos.DrawWireSphere(position, 0.1f);
-            RaycastHit2D hit = Physics2D.CircleCast(position, 0.1f, direction, distance);
+            RaycastHit2D hit = Physics2D.CircleCast(position + direction * 0.1f, 0.1f, direction);
+
             if (hit != default(RaycastHit2D))
             {
                 disToHit = hit.distance;
                 normal = hit.normal;
+                Debug.Log($"{normal} | {hit.collider.transform.position}");
+                Vector3 contactPoint = position + direction * disToHit + direction * 0.1f;
+                RaycastHit2D hit2 = Physics2D.Raycast(contactPoint, Average(Vector2.Reflect(direction, normal), direction), 0.1f);
+                if (hit2 != default(RaycastHit2D))
+                {
+                    normal = Average(normal, hit2.normal);
+                    DrawSphere(hit2.point, Color.red);
+                }
+
+               DrawSphere(hit.point, Color.red);
             }
             else
             {
                 disToHit = distance;
             }
 
-            Gizmos.DrawRay(position, direction * disToHit);
-            position += direction * disToHit;
+            Gizmos.DrawRay(position, direction * disToHit + direction * 0.1f);
+            position += direction * disToHit + direction * 0.1f;
             Gizmos.DrawWireSphere(position, 0.1f);
 
             distance -= disToHit;
 
             if (normal != Vector2.zero)
-                direction = Vector3.Reflect(direction, normal);
+            {
+                Vector3 newDirection = Vector3.Reflect(direction, normal);
+                if (newDirection == direction)
+                    direction = Average(direction, normal);
+                else if (newDirection == Vector3.zero)
+                    direction *= -1;
+                else
+                    direction = newDirection;
 
-            if (i++ > 100)
+                log.AppendLine($"newDirection => {direction}");
+            }
+
+            if (++i >= gizmosMaxContact)
                 break;
         }
-        Debug.Log($"i: {i}");
+        log.AppendLine($"i: {i}");
+        Debug.Log(log);
+    }
+
+    private void DrawSphere(Vector3 position, Color color)
+    {
+        Color tmp = Gizmos.color;
+        Gizmos.color = color;
+        Gizmos.DrawWireSphere(position, 0.1f);
+        Gizmos.color = tmp;
+    }
+
+    private Vector2 Average(params Vector2[] vectors)
+    {
+        float x = 0f;
+        float y = 0f;
+        foreach (var v in vectors)
+        {
+            x += v.x;
+            y += v.y;
+        }
+        x /= vectors.Count();
+        y /= vectors.Count();
+        return new Vector2(x, y).normalized;
     }
 }
