9a06ec8eeee1b1c63423d0593f9687f845ea5d24
Marikuana <marians22092000@gmail.com>
2022-05-24 18:22:30 +0300

    update UI Controller

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -5,15 +5,58 @@ using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
-    [SerializeField] private Controller controller;
+    [SerializeField] 
+    private Controller controller;
 
+    [SerializeField]
+    private Text bulletCount;
+    [SerializeField]
+    private Text moneyLabel;
 
-    public void RegenerateBricks()
+    [SerializeField]
+    private Button restartLevel;
+    [SerializeField]
+    private Button pause;
+
+    private void Awake()
     {
+        SetMoney(Manager.Instance.ProgressData.Money);
+        Manager.Instance.ProgressData.OnMoneyUpdate += SetMoney;
+    }
+
+    private void OnDestroy()
+    {
+        Manager.Instance.ProgressData.OnMoneyUpdate -= SetMoney;
+    }
+
+    private void Start()
+    {
+        restartLevel.onClick.AddListener(RestartLevel);
+        pause.onClick.AddListener(Pause);
+    }
+
+    private void Update()
+    {
+        SetBulletCount(Launcher.Balls);
+    }
+
+    public void RestartLevel()
+    {
+        controller.RestartLevel();
     }
 
     public void Pause()
     {
         controller.TogglePause();
+    }
+
+    private void SetBulletCount(int value)
+    {
+        bulletCount.text = $"Bullets: {value}";
+    }
+
+    private void SetMoney(int money)
+    {
+        moneyLabel.text = money.ToString();
     }
 }
