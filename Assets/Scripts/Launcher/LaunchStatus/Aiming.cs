97b8bfb2dcad5d23e614c5207f6007120c50d367
Marikuana <marians22092000@gmail.com>
2022-05-30 14:50:54 +0300

    fix launcher

---
+++ Assets/Scripts/Launcher/LaunchStatus/Aiming.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Aiming : LaunchStatus
+{
+    private Vector2 launchVector = Vector2.up;
+
+    public Aiming(Launcher launcher) : base(launcher)
+    {
+        launcher.RenderLine(true);
+    }
+
+    public override void Update()
+    {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        launchVector = (cursorPos - (Vector2)launcher.transform.position).normalized;
+
+        float lineLenght = Mathf.Clamp(Vector3.Distance(launcher.transform.position, cursorPos), 2, 7);
+        launcher.SetLinePoistion(launchVector * lineLenght);
+
+        if (CanShoot())
+            launcher.SetLineColor(Color.yellow);
+        else
+            launcher.SetLineColor(Color.red);
+
+        if (!Input.GetButton("Fire1"))
+        {
+            if (CanShoot())
+                launcher.status = new Launch(launcher, launchVector);
+            else
+                launcher.status = new ReadyToLaunch(launcher);
+        }
+
+    }
+
+    public override void Restart()
+    {
+
+    }
+
+    private bool CanShoot() =>
+        launchVector.y > 0.3f;
+}
--- Assets/Scripts/Launcher/LaunchStatus/ReadyToLaunch.cs
+++ Assets/Scripts/Launcher/LaunchStatus/ReadyToLaunch.cs
@@ -3,41 +3,27 @@ using UnityEngine.EventSystems;
 
 public class ReadyToLaunch : LaunchStatus
 {
-    private Vector2 launchVector = Vector2.up;
+    private int pointerId = -1;
     public ReadyToLaunch(Launcher launcher) : base(launcher)
     {
+#if !UNITY_EDITOR
+        pointerId = 0;
+#endif
+        launcher.RenderLine(false);
     }
 
 
     public override void Update()
     {
-        if ((Input.GetButton("Fire1") || Input.GetButtonUp("Fire1")) && EventSystem.current.IsPointerOverGameObject())
-            return;
-
-        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        launchVector = (cursorPos - (Vector2)launcher.transform.position).normalized;
-
-        float lineLenght = Mathf.Clamp(Vector3.Distance(launcher.transform.position, cursorPos), 2, 7);
-        launcher.SetLinePoistion(launchVector * lineLenght);
-
-
-        launcher.RenderLine(Input.GetButton("Fire1"));
-
-        if (CanShoot())
+        if (Input.GetButtonDown("Fire1") && !EventSystem.current.IsPointerOverGameObject(pointerId))
         {
-            if (Input.GetButtonUp("Fire1"))
-                launcher.status = new Launch(launcher, launchVector);
-            launcher.SetLineColor(Color.yellow);
+            launcher.status = new Aiming(launcher);
         }
-        else
-            launcher.SetLineColor(Color.red);
     }
 
-    private bool CanShoot() =>
-        launchVector.y > 0.3f;
+    
 
     public override void Restart()
     {
-
     }
 }
