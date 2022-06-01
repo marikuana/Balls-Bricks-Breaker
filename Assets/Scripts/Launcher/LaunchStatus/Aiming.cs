bfe70659013b71a7c21b4b3210e306340a723599
Marikuana <marians22092000@gmail.com>
2022-06-01 16:02:21 +0300

    refactoring

--- Assets/Scripts/Launcher/LaunchStatus/Aiming.cs
+++ Assets/Scripts/Launcher/LaunchStatus/Aiming.cs
@@ -2,41 +2,50 @@
 
 public class Aiming : LaunchStatus
 {
-    private Vector2 launchVector = Vector2.up;
-
     public Aiming(Launcher launcher) : base(launcher)
     {
+        SetLineEndPosition(GetCursorPosition());
         launcher.RenderLine(true);
     }
 
     public override void Update()
     {
-        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        launchVector = (cursorPos - (Vector2)launcher.transform.position).normalized;
+        Vector2 cursorPos = GetCursorPosition();
+        SetLineEndPosition(cursorPos);
 
-        float lineLenght = Mathf.Clamp(Vector3.Distance(launcher.transform.position, cursorPos), 2, 7);
-        launcher.SetLinePoistion(launchVector * lineLenght);
+        Vector2 launchDirection = GetLaunchDirection(cursorPos);
 
-        if (CanShoot())
+        if (CanShoot(launchDirection))
             launcher.SetLineColor(Color.yellow);
         else
             launcher.SetLineColor(Color.red);
 
         if (!Input.GetButton("Fire1"))
         {
-            if (CanShoot())
-                launcher.status = new Launch(launcher, launchVector);
+            if (CanShoot(launchDirection))
+                launcher.status = new Launch(launcher, launchDirection);
             else
                 launcher.status = new ReadyToLaunch(launcher);
         }
-
     }
+
+    private void SetLineEndPosition(Vector2 position)
+    {
+        float lineLenght = Mathf.Clamp(Vector3.Distance(launcher.transform.position, position), 2, 7);
+        launcher.SetLinePoistion(GetLaunchDirection(position) * lineLenght);
+    }
+
+    private Vector2 GetLaunchDirection(Vector2 cursorPosition) =>
+        (cursorPosition - (Vector2)launcher.transform.position).normalized;
+
+    private Vector2 GetCursorPosition() =>
+        Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
     public override void Restart()
     {
-
+        launcher.RenderLine(false);
     }
 
-    private bool CanShoot() =>
-        launchVector.y > 0.3f;
+    private bool CanShoot(Vector2 launchDirection) =>
+        launchDirection.y > 0.3f;
 }
